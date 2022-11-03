289acfbf54967c7a111c182a7719420162e4f4fa
Isabella-Amantino <isabella.amantino@hotmail.com>
2022-11-03 15:41:52 -0300

    criação about

--- daterra/src/App.tsx
+++ daterra/src/App.tsx
@@ -4,25 +4,27 @@ import Footer from "./components/estaticos/footer/Footer";
 import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
 import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
+import About from "./paginas/about/About";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 
 function App() {
-  return(
+  return (
     <Router>
-    <Navbar />
-    <div style={{ minHeight: '100vh' }}>
-    <Routes> // Antigo Switch
-    <Route path="/" element={<Login />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    <Route path="/cadastro" element={<CadastroUsuario />} />
-    </Routes>
-    </div>
-    <Footer />
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
+          <Route path="/about"  element={<About />}/>
+        </Routes>
+      </div>
+      <Footer />
     </Router>
-    )
+  )
 }
 
 export default App;
