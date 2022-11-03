9488094521416f2b288511933f3ad99507144d96
Delfina8 <githubdelfina@gmail.com>
2022-11-03 18:37:00 -0300

    navbar

--- daterra/src/App.tsx
+++ daterra/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "./components/estaticos/navbar/Navbar";
-import Footer from "./components/estaticos/footer/Footer";
+/*import Footer from "./components/estaticos/footer/Footer";
 import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
-import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";*/
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+/*  <Route path="/" element={<Login />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/cadastro" element={<CadastroUsuario />}
+    <Footer /> />*/
 
 function App() {
   return(
@@ -14,13 +19,10 @@ function App() {
     <Navbar />
     <div style={{ minHeight: '100vh' }}>
     <Routes> // Antigo Switch
-    <Route path="/" element={<Login />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    <Route path="/cadastro" element={<CadastroUsuario />} />
+    
     </Routes>
     </div>
-    <Footer />
+    
     </Router>
     )
 }
--- daterra/src/components/estaticos/navbar/Navbar.tsx
+++ daterra/src/components/estaticos/navbar/Navbar.tsx
@@ -1,12 +1,88 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Box, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
+import { createStyles, alpha, Theme, makeStyles } from '@material-ui/core/styles';
+import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+
+        title: {
+            flexGrow: 1,
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+                display: 'block',
+            }
+        },
+    },
+    )
+);
+
+
 
 function Navbar() {
+
+    const classes = useStyles();
+
     return (
         <>
-        
+            <AppBar position="static">
+                <Toolbar variant="dense" className='corFundo'  >
+                    <Box className={classes.title}>
+                        <Box display="flex" justifyContent="start">
+                            <Toolbar>
+                                <img src="https://i.postimg.cc/Fs74VKBM/Logo-com-letras.png" alt="Logotipo" height={130} width={300} />
+                            </Toolbar>
+                        </Box>
+                        
+                    </Box>
+                    <Box display="flex" justifyContent="start" className={classes.title} style={{ paddingRight: "1rem" }}>
+                        
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" className="corTexto tamTexto">
+                                Quem Somos |
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor' style={{ paddingRight: "1rem" }}>
+                            <Typography variant="h6" className="corTexto tamTexto">
+                                Como Comprar |
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor' style={{ paddingRight: "1rem" }}>
+                            <Typography variant="h6" className="corTexto tamTexto">
+                                Meu Perfil |
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor' style={{ paddingRight: "1rem" }}>
+                            <Typography variant="h6" className="corTextoProdutos tamTexto">
+                                Todos Produtos |
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor' style={{ paddingRight: "1rem" }}>
+                            <Typography variant="h6" className="corTexto tamTexto">
+                                Cadastrar Produto |
+                            </Typography>
+                        </Box>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box display="flex" mx={1} className='cursor' style={{ paddingRight: "10rem" }}>
+                                <Typography variant="h6" className="corTexto tamTexto">
+                                    Sair
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Box display="flex" className='cursor'>
+                            <Toolbar>
+                                <img src="https://i.postimg.cc/15y2H2x6/pngegg.png" alt="sacola de compras" height={60} width={60} />
+                            </Toolbar>
+                        </Box>
+                    </Box>
+                </Toolbar>
+            </AppBar>
         </>
-
     );
 }
+
 
 export default Navbar;
