ebc3fd6a3885ac19493fdb8a022ee85f5be62fa1
cecilianakamura <ceci.n.nakamura@gmail.com>
2022-11-03 15:42:44 -0300

    Removendo placeholder dos demais componentes

--- daterra/src/App.tsx
+++ daterra/src/App.tsx
@@ -1,25 +1,25 @@
 import React from "react";
-// import Navbar from "./components/estaticos/navbar/Navbar";
-// import Footer from "./components/estaticos/footer/Footer";
-// import Home from "./paginas/home/Home";
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
+import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
-// import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
+      <Navbar />
       <div style = {{minHeight:'100vh'}}>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/login' element={<Login />} />
-          {/* <Route path='/home' element={<Home />} />
-          <Route path='/cadastro' element={<CadastroUsuario />} /> */}
+          <Route path='/home' element={<Home />} />
+          <Route path='/cadastro' element={<CadastroUsuario />} />
         </Routes>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </Router>
   );
 }
