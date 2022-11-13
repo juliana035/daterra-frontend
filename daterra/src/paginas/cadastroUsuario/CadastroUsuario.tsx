cf8b87fffc30c510844d8e33e68b7e7978eaa38e
cecilianakamura <ceci.n.nakamura@gmail.com>
2022-11-13 20:01:20 -0300

    Adicionando cards de tipoUser

--- daterra/src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ daterra/src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
 import './CadastroUsuario.css';
-import { Box } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, CardMedia, Radio } from '@mui/material';
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import User from "../../models/User";
@@ -69,37 +69,105 @@ function CadastroUsuario() {
         }
     }
 
-    return (
-        <Grid container direction="row" justifyContent="center" alignItems="center">
-            <Grid item xs={6} className='imagem2'></Grid>
-            <Grid item xs={6} alignItems='center' >
-                <Box paddingX={10}>
-                    <form onSubmit={onSubmit}>
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='textos2'>Cadastrar</Typography>
-                        <TextField value={user.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="nome" label="Nome" variant="outlined" name="nome" margin="normal" fullWidth />
-                        <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="usuario" label="Usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
-                        <TextField value={user.cep} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="cep" label="CEP" variant="outlined" name="cep" margin="normal" fullWidth />
-                        <TextField value={user.cpnj} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="cpnj" label="CNPJ" variant="outlined" name="cpnj" margin="normal" fullWidth />
-                        <TextField value={user.cpf} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="cpf" label="CPF" variant="outlined" name="cpf" margin="normal" fullWidth />
-                        <TextField value={user.tipoUser} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="tipoUser" label="Tipo de Usuário" variant="outlined" name="tipoUser" margin="normal" fullWidth />
-                        <TextField value={user.foto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="foto" label="URL da foto" variant="outlined" name="foto" margin="normal" fullWidth />
-                        <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="senha" label="Senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
-                        <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id="confirmarSenha" label="confirmarSenha" variant="outlined" name="confirmarSenhaha" margin="normal" type="password" fullWidth />
-                        <Box marginTop={2} textAlign="center">
-                            <Link to="/login" className="text-decorator-none">
-                                <Button variant="contained" style={{ color: "white", borderColor: "white", backgroundColor: "#FF0000" }} className='btnCancelar'>
-                                    Cancelar
-                                </Button>
-                            </Link>
-                            <Button type='submit' variant="contained" style={{ color: "white", borderColor: "white", backgroundColor: "#FF731D" }}>
-                                Cadastrar
-                            </Button>
 
-                        </Box>
-                    </form>
-                </Box>
+    const [selectedValue, setSelectedValue] = React.useState('produtor');
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedValue(event.target.value);
+    }
+
+    return (
+        <Grid className="cadastroUsuarioFundo">
+
+            <Grid className='cadastroUsuariotela1'>
+                <Typography variant='h3' component='h3' className='cadastroUsuarioTitulo1'>
+                    Selecione o seu perfil:
+                </Typography>
+
+                <Card className='cadastroUsuarioTipoUser'>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            src="https://i.imgur.com/zoQy7wV.jpg"
+                            alt="trabalhador rural segurando uma cesta com hortaliças"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h6" component="div" className='cadastroUsuarioTextoTela1'>
+                                Produtor
+                            </Typography>
+                            <Typography variant="body2" className='cadastroUsuarioTextoTela1'>
+                                Sou um PRODUTOR e quero vender meus produtos nesta plataforma.
+                            </Typography>
+                            <Radio checked={selectedValue === 'produtor'} onChange={handleChange} value='produtor' name='radio-buttons' input-props={{ 'aria-label': 'PRODUTOR' }} />
+                        </CardContent>
+                </Card>
+
+                <Card className='cadastroUsuarioTipoUser'>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            src="https://i.imgur.com/pLG26wm.jpg"
+                            alt="refeitório empresarial"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" component="div" className='cadastroUsuarioTextoTela1'>
+                                Consumidor - Empresa
+                            </Typography>
+                            <Typography variant="body2" className='cadastroUsuarioTextoTela1'>
+                                Sou uma EMPRESA e quero fornecer alimentos orgânicos para meus colaboradores.
+                            </Typography>
+                            <Radio checked={selectedValue === 'cpnj'} onChange={handleChange} value='cpnj' name='radio-buttons' input-props={{ 'aria-label': 'CPNJ' }} />
+                        </CardContent>
+                </Card>
+                <Card className='cadastroUsuarioTipoUser'>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            src="https://i.imgur.com/miQ9Ulf.jpg"
+                            alt="família cozinhando"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" component="div" className='cadastroUsuarioTextoTela1'>
+                                Consumidor - Pessoa Física
+                            </Typography>
+                            <Typography variant="body2" className='cadastroUsuarioTextoTela1'>
+                                Sou uma PESSOA FÍSICA e quero inserir produtos orgânicos na minha alimentação.
+                            </Typography>
+                            <Radio checked={selectedValue === 'cpf'} onChange={handleChange} value='cpf' name='radio-buttons' input-props={{ 'aria-label': 'CPF' }} />
+                        </CardContent>
+                </Card>
             </Grid>
 
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid item xs={6} className='imagem2'></Grid>
+                <Grid item xs={6} alignItems='center' >
+                    <Box paddingX={10}>
+                        <form onSubmit={onSubmit}>
+                            <Typography gutterBottom variant='h3' component='h3' className='cadastroUsuariotextos2'>Cadastrar</Typography>
+                            <TextField value={user.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="nome" label="Nome" variant="outlined" name="nome" margin="normal" fullWidth />
+                            <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="usuario" label="Usuário" variant="outlined" name="usuario" margin="normal" fullWidth />
+                            <TextField value={user.cep} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="cep" label="CEP" variant="outlined" name="cep" margin="normal" fullWidth />
+                            <TextField value={user.cpnj} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="cpnj" label="CNPJ" variant="outlined" name="cpnj" margin="normal" fullWidth />
+                            <TextField value={user.cpf} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="cpf" label="CPF" variant="outlined" name="cpf" margin="normal" fullWidth />
+                            <TextField value={user.tipoUser} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="tipoUser" label="Tipo de Usuário" variant="outlined" name="tipoUser" margin="normal" fullWidth />
+                            <TextField value={user.foto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="foto" label="URL da foto" variant="outlined" name="foto" margin="normal" fullWidth />
+                            <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="senha" label="Senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
+                            <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id="confirmarSenha" label="confirmarSenha" variant="outlined" name="confirmarSenhaha" margin="normal" type="password" fullWidth />
+                            <Box marginTop={2} textAlign="center">
+                                <Link to="/login" className="text-decorator-none">
+                                    <Button variant="contained" style={{ color: "white", borderColor: "white", backgroundColor: "#FF0000" }} className='btnCancelar'>
+                                        Cancelar
+                                    </Button>
+                                </Link>
+                                <Button type='submit' variant="contained" style={{ color: "white", borderColor: "white", backgroundColor: "#FF731D" }}>
+                                    Cadastrar
+                                </Button>
+
+                            </Box>
+                        </form>
+                    </Box>
+                </Grid>
+
+            </Grid>
         </Grid>
     );
 }
