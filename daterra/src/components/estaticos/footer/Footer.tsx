83c00a26a12a8960bf1e00b2b1998d33f7a50f06
cecilianakamura <ceci.n.nakamura@gmail.com>
2022-11-16 09:15:08 -0300

    Adicionando renderização condicional do footer

--- daterra/src/components/estaticos/footer/Footer.tsx
+++ daterra/src/components/estaticos/footer/Footer.tsx
@@ -1,52 +1,57 @@
-
-
 import React from 'react';
-
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Grid, Button } from '@material-ui/core';
 import {Box} from '@mui/material';
-
 import "./Footer.css";
-
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
 
 function Footer() {
+    const token = useSelector<TokenState,TokenState['tokens']>(
+        (state) => state.tokens
+    );
+    
     return (
+        <>
+        {
+            token!==''&&<Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid alignItems="center" item xs={12}>
+                <Box>
 
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box>
-
-                    {/* <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography> */}
-                        </Box>
-                        <Box className='box1'>
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon className='redes'/>
-                            </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon className='redes'/>
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon className='redes'/>
-                            </a>
-                            <Button type='submit' variant='contained' className='bt-feira'>
-                                    IR PARA FEIRINHA
-                                </Button>
-                        </Box>
-
-                            
-                        
-                     
-                    <Box className='box2'>
-                        <Box>
-                        <Typography variant="subtitle2" align='center' className='footerCopyright' >© 2022 Todos os Direitos Reservados  </Typography>
-                        </Box>
+                {/* <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography> */}
+                    </Box>
+                    <Box className='box1'>
+                        <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
+                            <FacebookIcon className='redes'/>
+                        </a>
+                        <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
+                            <InstagramIcon className='redes'/>
+                        </a>
+                        <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
+                            <LinkedInIcon className='redes'/>
+                        </a>
+                        <Button type='submit' variant='contained' className='bt-feira'>
+                                IR PARA FEIRINHA
+                            </Button>
                     </Box>
 
+                        
+                    
+                 
+                <Box className='box2'>
+                    <Box>
+                    <Typography variant="subtitle2" align='center' className='footerCopyright' >© 2022 Todos os Direitos Reservados  </Typography>
+                    </Box>
+                </Box>
 
-                </Grid>
+
             </Grid>
+        </Grid>
+        }
+        </>
+            
 
     )
 
